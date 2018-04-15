4acb4256b37e9c26cb01f6569182177f766671f8
David Lannoye <david@lannoye.net>
2018-04-15 16:31:32 -0700

    Fix missing .on in exercise 11

--- src/exercises/11.js
+++ src/exercises/11.js
@@ -91,12 +91,12 @@ class Toggle extends React.Component {
   reset = () =>
     this.internalSetState(
       {...this.initialState, type: Toggle.stateChangeTypes.reset},
-      () => this.props.onReset(this.getState()),
+      () => this.props.onReset(this.getState().on),
     )
   toggle = ({type = Toggle.stateChangeTypes.toggle} = {}) =>
     this.internalSetState(
       ({on}) => ({type, on: !on}),
-      () => this.props.onToggle(this.getState()),
+      () => this.props.onToggle(this.getState().on),
     )
   getTogglerProps = ({onClick, ...props} = {}) => ({
     onClick: callAll(onClick, () => this.toggle()),
