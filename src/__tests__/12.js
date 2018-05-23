aaa0f8cfa2d37b859117e970e7a2df09486f2087
Martin Gostisha <martin@flipgrid.com>
2018-05-23 14:36:09 -0500

    Fix e query param in elaboration and feedback link

--- src/__tests__/12.js
+++ src/__tests__/12.js
@@ -81,7 +81,7 @@ test('handles static properties', () => {
 // 3. Change submitted from `false` to `true`
 // 4. And you're all done!
 /*
-http://ws.kcd.im/?ws=react%20patterns&e=11&em=
+http://ws.kcd.im/?ws=react%20patterns&e=12&em=
 */
 test.skip('I submitted my elaboration and feedback', () => {
   const submitted = false // change this when you've submitted!
