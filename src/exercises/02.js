dc75f3e4ac2c733b74156e815ad24d9a5b66dae2
Kent C. Dodds <kent+github@doddsfamily.us>
2018-09-04 10:32:51 -0600

    add extra credit for 02
    Closes #43

--- src/exercises/02.js
+++ src/exercises/02.js
@@ -38,6 +38,9 @@ class Toggle extends React.Component {
   }
 }
 
+// 💯 Support rendering non-Toggle components within Toggle without incurring warnings in the console.
+// for example, try to render a <span>Hello</span> inside <Toggle />
+
 // Don't make changes to the Usage component. It's here to show you how your
 // component is intended to be used and is used in the tests.
 // You can make all the tests pass by updating the Toggle component.
