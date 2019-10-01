2fbe1ad786a61ddc40f2c30d64b1c3b2a892f446
Thomas Joaquini <thomas.joaquini@gmail.com>
2019-10-01 16:04:56 -0300

    first exercise solution

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {renderToggle} from '../../test/utils'
-import Usage from '../exercises-final/01'
-// import Usage from '../exercises/01'
+// import Usage from '../exercises-final/01'
+import Usage from '../exercises/01'
 
 test('renders a toggle component', () => {
   const handleToggle = jest.fn()
@@ -24,8 +24,8 @@ test('renders a toggle component', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=01&em=thomas.joaquini@gmail.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
--- src/exercises/01.js
+++ src/exercises/01.js
@@ -3,7 +3,7 @@
 import React from 'react'
 // 🐨 uncomment this import to get the switch component.
 // It takes an `onClick` and an `on` prop
-// import {Switch} from '../switch'
+import {Switch} from '../switch'
 
 class Toggle extends React.Component {
   // 🐨 this toggle component is going to need to have state for `on`
@@ -11,9 +11,24 @@ class Toggle extends React.Component {
   // You'll also want a method to handle when the switch is clicked
   // which will update the `on` state and call the `onToggle` prop
   // 💰 this.props.onToggle(this.state.on)
+  state = {
+    on: false,
+  };
+
+  toggle = () => 
+    this.setState(
+      ({ on }) => ({ on: !on }),
+      () => {
+        this.props.onToggle(this.state.on);
+      },
+    );
+
   render() {
     // 🐨 here you'll want to return the switch with the `on` and `onClick` props
-    return null
+    const { on } = this.state;
+    return (
+      <Switch on={on} onClick={this.toggle} />
+    )
   }
 }
 
